65858709c2cbaffc21308724ab1ee3edc398f4a1
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-07-26 23:11:24 +0900

    add: nativewind-tailwindcss 라이브러리 추가

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+// App.tsx 또는 app/index.tsx 등
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-5xl text-blue-600">Hello Tailwind CSS!</Text>
+    </View>
+  );
+}
